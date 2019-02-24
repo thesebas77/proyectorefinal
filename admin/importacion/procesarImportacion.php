ad068575f31515c35a8f567763e10ed6ca8f6fc5
unknown <cascosole@gmail.com>
2019-02-23 21:58:25 -0300

    Agregue la el menu

--- admin/importacion/procesarImportacion.php
+++ admin/importacion/procesarImportacion.php
@@ -1,7 +1,14 @@
+<?php 
+     include '../extend/header.php';
+     include '../extend/permiso.php';
+              
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <title>Documento sin título</title>
 </head>
 
@@ -13,7 +20,7 @@ $truncate=$con->prepare("truncate table valuacion");
 $truncate->execute();
 if($truncate)
 {
-   echo"borro la valuacion anterior y ";
+   echo"<p>Se borro la valuacion anterior y ";
 }
 
 $ruta = 'Upload/';
@@ -28,8 +35,8 @@ foreach ($_FILES as $key) {
   $sql='LOAD DATA LOCAL INFILE "'.$destino.'" INTO TABLE valuacion FIELDS TERMINATED BY \';\'
    LINES TERMINATED BY \'\n\' (origen,cod_fabrica , cod_marca, cod_tipo,cod_modelo,tv,marca,descripcion,tipo,val_okm,val_1,val_2,val_3,val_4,val_5,val_6,val_7,val_8,val_9,val_10,val_11,val_12,val_13,val_14,val_15,val_16,val_17,val_18,val_19,val_20,val_21,val_22,val_23,val_24)'; 
 $res=mysqli_query($con,$sql);
-$affected = (int) (mysqli_affected_rows($con))-1;
-echo "Se Insertaron ",$affected," nuevos registros";
+$affected = (int) (mysqli_affected_rows($con));
+echo "Se Insertaron ",$affected," nuevos registros</p>";
 
 ?>
 </body>
