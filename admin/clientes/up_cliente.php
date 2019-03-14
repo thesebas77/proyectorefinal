20bb22d45e0cab7bceb96765a528ee5cde01906e
unknown <cascosole@gmail.com>
2019-03-14 17:58:44 -0300

    update propietario

--- admin/clientes/up_cliente.php
+++ admin/clientes/up_cliente.php
@@ -1,33 +1,32 @@
 <?php include '../conexion/conexion.php'; 
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
-		
-		$nume = htmlentities($_POST['nume']);
-		$dni = htmlentities($_POST['dni']);
-		$cuit = htmlentities($_POST['cuil']);
-		$nom = htmlentities($_POST['nom']);
-		$ape = htmlentities($_POST['ape']);
-		$razon=htmlentities($_POST['ape']);
-		$dire = htmlentities($_POST['dire']);
-		$email = htmlentities($_POST['email']);
-		$ciu = htmlentities($_POST['ciu']);
-		$tipo=htmlentities($_POST['email']);
-		$grupo = htmlentities($_POST['email']);
-		$obs = htmlentities($_POST['observacion']);
+		if(isset($_POST['nume'])){$nume = htmlentities($_POST['nume']);}
+		if(isset($_POST['dni'])){$dni = htmlentities($_POST['dni']);}
+		if(isset($_POST['cuit'])){$cuit = htmlentities($_POST['cuit']);}
+		if(isset($_POST['nom'])){$nom = htmlentities($_POST['nom']);}
+		if(isset($_POST['ape'])){$ape = htmlentities($_POST['ape']);}
+		if(isset($_POST['razon'])){$razon=htmlentities($_POST['razon']);}
+		if(isset($_POST['dire'])){$dire = htmlentities($_POST['dire']);}
+		if(isset($_POST['email'])){$email = htmlentities($_POST['email']);}
+		if(isset($_POST['ciu'])){$ciu = htmlentities($_POST['ciu']);}
+		if(isset($_POST['tipo'])){$tipo=htmlentities($_POST['tipo']);}
+		if(isset($_POST['grupo'])){$grupo = htmlentities($_POST['grupo']);}
+		if(isset($_POST['observacion'])){$obs = htmlentities($_POST['observacion']);}
 
-
-		if (empty($dni)){
+		if ($dni){
 			$tipo = 'Juridica';
 			$ins = $con -> prepare("UPDATE propietario SET  razonSocial=?, cuit=?, domicilio=?, email=?, localidad=?, tipo=?,grupo=?,estado=?, observaciones=? WHERE id = ?");
-			$ins -> bind_param('sisssssiss',$razon,$cuit,$razon,$dire,$email,$ciu,$tipo,$gru,$est,$obs,$nume);
+			$ins -> bind_param('sissssissi',$razon,$cuit,$dire,$email,$ciu,$tipo,$gru,$est,$obs,$nume);
+			$ins -> execute();
 		}else{
 			$tipo = 'Humana';
 			$ins = $con -> prepare("UPDATE propietario SET  nombre=?, apellido=?,  dni=?, domicilio=?, email=?, localidad=?, tipo=?,grupo=?,estado=?, observaciones=? WHERE id = ?");
-			$ins -> bind_param('ssissssiss',$nom,$ape,$dni,$dire,$email,$ciu,$tipo,$gru,$est,$obs,$nume);
+			$ins -> bind_param('ssissssissi',$nom,$ape,$dni,$dire,$email,$ciu,$tipo,$gru,$est,$obs,$nume);
+			$ins -> execute();
 		}
 		
-		$ins -> execute();
-		$ins -> close();
+		
 
 		if($ins){
 			header('location:../extend/alerta.php?msj=Se ha actualizado el propietario con exito&c=cl&p=lic&t=success');
@@ -36,7 +35,7 @@
 		}else{
 
 			header('location:../extend/alerta.php?msj=No se ha podido actualizar el propietario&c=cl&p=lic&t=error');
-
+			echo "error";
 		}
 
 		
